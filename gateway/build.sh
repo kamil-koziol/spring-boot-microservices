3ee89e74c94c9f6ccc7c20d5f942511d7278ff0c
Kamil Kozioł <38388156+kamil-koziol@users.noreply.github.com>
2024-04-01 22:55:17 +0200

    docker (#6)

---
+++ gateway/build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Builds application and docker image using Dockerfile and tags it based on org.opencontainers.image.version label in
+# Dockerfile.
+
+#######################################
+# Script main function. Builds applicaiton and docker image using Dockerfile and tags it based on
+# org.opencontainers.image.version label in Dockerfile.
+# Arguments:
+#   None.
+#######################################
+
+
+function main() {
+    ./mvnw clean verify
+    title="gateway"
+    version="1.0.0"
+    docker build \
+      --label "org.opencontainers.image.created=$(date +%Y-%m-%dT%H:%M:%S%:z)" \
+      --label "org.opencontainers.image.ref.name=$(git rev-parse HEAD)" \
+      --label "org.opencontainers.image.revision=$(git rev-parse HEAD)" \
+      -t "${title}":"${version}" .
+}
+
+main "$@"
